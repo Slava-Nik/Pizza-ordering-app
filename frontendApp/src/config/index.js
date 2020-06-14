6ba48527363b61394dbf2814104b47106770551c
Slava-Nik <lancerokey@yandex.ru>
2020-06-14 21:11:55 +0300

    updated frontend config for production

--- frontendApp/src/config/index.js
+++ frontendApp/src/config/index.js
@@ -11,9 +11,9 @@ const config = {
     },
   },
   production: {
-    backend: "http://localhost:5000",
+    backend: "http://13.59.241.189:5000",
     axios: {
-      baseURL: "http://localhost:5000/api/v1",
+      baseURL: "http://13.59.241.189/api/v1",
       withCredentials: true,
       headers: {
         "Content-Type": "application/json",
