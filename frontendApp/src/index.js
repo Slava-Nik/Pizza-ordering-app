a0b35dba08952b44ce7e2b70bd5c2bd6466a9cf7
Slava-Nik <lancerokey@yandex.ru>
2020-06-15 00:31:17 +0300

    corrected modals for landspace orientation + added code splitting for client routing

--- frontendApp/src/index.js
+++ frontendApp/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "@components/Header";
 import { PersistGate } from "redux-persist/integration/react";
-import Home from "./router/pages/Home";
-import Cart from "./router/pages/Cart";
 import configureStore from "./store/configureStore";
-
 import "./common/styles/app.scss";
+
+const Home = lazy(() => import("./router/pages/Home"));
+const Cart = lazy(() => import("./router/pages/Cart"));
 
 function App() {
   const { store, persistor } = configureStore();
@@ -17,15 +17,13 @@ function App() {
       <PersistGate loading={null} persistor={persistor}>
         <Router>
           <div className="main-wrapper">
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/cart">
-                <Cart />
-              </Route>
-            </Switch>
+            <Suspense fallback={null}>
+              <Header />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/cart" component={Cart} />
+              </Switch>
+            </Suspense>
           </div>
         </Router>
       </PersistGate>
